0f2c84dbcdc6c05f0d916b7f6dffc3135e01b347
Ananya <ananya.joshi@sjsu.edu>
2023-03-10 20:37:24 -0800

    Added confusion matrix, classifier, F1 score

--- pipeline.py
+++ pipeline.py
@@ -19,16 +19,36 @@ initial_low = viz1['infection_level'].value_counts()[0]
 initial_mod = viz1['infection_level'].value_counts()[2]
 initial_high = viz1['infection_level'].value_counts()[1]
 
+classifier_name = [
+    'Gaussian Naive Bayes',
+    'KNeighbors Classifier',
+    'Support Vector Classifier', 
+    'Decision Tree Classifier',
+    'Random Forest Classifier',
+    'AdaBoost Classifier',
+    'Gradient Boosting Classifier'
+]
+classifiers = [
+    GaussianNB(),
+    KNeighborsClassifier(algorithm='ball_tree', n_neighbors=4),
+    SVC(kernel='rbf', C=0.05, random_state=42),
+    DecisionTreeClassifier(max_depth=2, random_state=42),
+    RandomForestClassifier(n_estimators=10, max_depth=2, random_state=42),
+    AdaBoostClassifier(n_estimators=10, learning_rate=0.01, random_state=42),
+    GradientBoostingClassifier(n_estimators=10, max_depth=2, learning_rate=0.1, random_state=42)
+]
+
 def scaling(df):
 
   # Create train and test set
-  df = pd.read_csv(df)
+  # df = pd.read_csv(df)
   X = df.drop(['area', 'infection_level'], axis=1)
   y = df['infection_level']
 
   # Scaling
   sc = MinMaxScaler()
   X_scaled = sc.fit_transform(X)
+  X_scaled = pd.DataFrame(X_scaled, columns=X.columns)
 
   return X_scaled, y
 
@@ -44,14 +64,12 @@ def compute_samples(mod_percent, high_percent):
 def oversample(mod_percent, high_percent, df):
 
   X_scaled, y = scaling(df)
-  print(mod_percent)
-  print(high_percent)
   low, mod, high = compute_samples(mod_percent, high_percent)
   
   # Minority oversampling and shuffling of dataset
   s_dict = {'Low':low, 'Moderate':mod, 'High':high}
   sm = SMOTE(sampling_strategy=s_dict, random_state=42)
-  X_res, y_res = sm.fit_resample(pd.DataFrame(X_scaled), y)
+  X_res, y_res = sm.fit_resample(pd.DataFrame(X_scaled, columns=X_scaled.columns), y)
 
   # Concat for reshuffling
   df_res = pd.concat([X_res, y_res], axis=1)
@@ -61,3 +79,49 @@ def oversample(mod_percent, high_percent, df):
   X_res, y_res = df_res.iloc[:, :-1], df_res.iloc[:, -1]
 
   return X_res, y_res
+
+def top_features(X: pd.DataFrame, y:pd.Series):
+  # X_scaled = scaling(X)
+  rf_clf = RandomForestClassifier(min_samples_split=5, random_state=1)
+  rf_clf.fit(X, y)
+
+  imp_features = pd.Series(data=rf_clf.feature_importances_, index=X.columns).sort_values(ascending=False)
+
+  top3 = list(imp_features.index[:3])
+  top10 = list(imp_features.index[:10])
+
+  return top3, top10
+
+def choose_best_classifier(X, y, classifier_name, classifiers):
+
+  max_weigted_score = 0.0
+
+  for name, clf in zip(classifier_name, classifiers):
+    cv_results = cross_validate(clf, X, y, cv=15, scoring=('f1_weighted'), return_train_score=True)
+
+    train_score = np.mean(cv_results['train_score'])
+    test_score = np.mean(cv_results['test_score'])
+
+    model_weighted_score = ((0.8*test_score)+(0.2*train_score))/2
+   
+    if model_weighted_score >= max_weigted_score:
+      max_weigted_score = model_weighted_score
+      classifier_name = name
+
+  return classifier_name
+
+def controller(mod_percent, high_percent, df):
+  X, y = oversample(mod_percent, high_percent, df)
+  top3, top10 = top_features(X, y)
+  classifier = choose_best_classifier(X, y, classifier_name, classifiers)
+
+  idx = classifier_name.index(classifier)
+
+  model = classifiers[idx]
+  model.fit(X, y)
+  pred = model.predict(X)
+
+  f1 = f1_score(y, pred, average='weighted')
+  cf = confusion_matrix(y, pred)
+  print(cf)
+  return f1, cf, y.value_counts(), y, top3, classifier
